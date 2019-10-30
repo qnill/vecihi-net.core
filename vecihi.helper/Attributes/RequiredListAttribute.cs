326f10a29639fd7058dcb0e78782035246a8c46a
Anıl Yıldırım <anilyildirim12@gmail.com>
2019-10-30 10:39:04 +0300

    Added required list att.

---
+++ vecihi.helper/Attributes/RequiredListAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace vecihi.helper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class RequiredListAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is IList list)
+                return list.Count > 0;
+
+            return false;
+        }
+    }
+}
